226557c7555641e25bf53a1e04ca05737fd3eaf5
Arman Habib <32464369+armanadd01@users.noreply.github.com>
2021-11-01 03:44:05 +0600

    Passing data test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
-const objectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const cors = require('cors');
 
@@ -22,7 +22,8 @@ async function run(){
         // console.log('connected')
         const database = client.db("TravelSiteAssignment");
         const PlansCollection = database.collection("plans");
-        const OrdersCollection = database.collection("orders")
+        const OrdersCollection = database.collection("orders");
+        const UsersCollection = database.collection("users");
 
         //Get API
         app.get('/plans', async(req, res) => {
@@ -31,9 +32,9 @@ async function run(){
             res.send(plan);
         });
         //GET Single API 
-        app.get('/plans/:id', async(req, res) => {
+        app.get('/addorder/:id', async(req, res) => {
             const id = req.params.id;
-            const query ={_id: objectId(id)};
+            const query ={_id: ObjectId(id)};
             const singlePlan = await PlansCollection.findOne(query);
              res.json(singlePlan);
         });
@@ -49,16 +50,68 @@ async function run(){
                  
                  res.json(result);
         });
+        //Add odders
         app.post("/addorder", async (req, res) => {
             console.log(req.body);
             const result = await OrdersCollection.insertOne(req.body);
             console.log("🚀 ~ file: index.js ~ line 55 ~ app.post ~ result", result)
             res.send(result);
           });
+
+            
+        // Get Order
+        app.get("/orders", async (req, res) => {
+            const result = await OrdersCollection.find({}).toArray();
+            res.send(result);
+            console.log(result);
+        });
+        // Delete order
+        app.delete("/deleteOrder/:id", async (req, res) => {
+            console.log(req.params.id);
+            const result = await OrdersCollection.deleteOne({
+              _id: ObjectId(req.params.id),
+            });
+            res.send(result);
+          });
+
+          app.get("/myorder/:email", async (req, res) => {
+            const result = await OrdersCollection.find({
+              email: req.params.email,
+            }).toArray();
+            console.log(result)
+            res.send(result);
+          });
+
+          //Add Users
+        app.post("/adduser", async (req, res) => {
+            console.log(req.body);
+            const result = await UsersCollection.insertOne(req.body);
+            console.log("🚀 ~ file: index.js ~ line 55 ~ app.post ~ result", result)
+            res.send(result);
+          });
+
+            
+        // Get Order
+        app.get("/users", async (req, res) => {
+            const result = await UsersCollection.find({}).toArray();
+            res.send(result);
+            console.log(result);
+        });
+        // Delete order
+        app.delete("/deleteusers/:id", async (req, res) => {
+            console.log(req.params.id);
+            const result = await UsersCollection.deleteOne({
+              _id: ObjectId(req.params.id),
+            });
+            res.send(result);
+          });
+
+          
+
         //Delete Single API 
         app.delete('/plans/:id', async(req, res) => {
             const id = req.params.id;
-            const query ={_id: objectId(id)};
+            const query ={_id: ObjectId(id)};
             const result = await PlansCollection.deleteOne(query);
              res.json(result);
 
